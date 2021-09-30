62dcbe6e4b983d82ee48eb8b45108a9c4fd79639
Aism793 <ismacenteno123@gmail.com>
2021-09-29 22:57:44 -0500

    Caso prueba NoPuedoDisminuirCantidadProductoDelCarrito

--- OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
+++ OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
@@ -186,8 +186,39 @@ namespace OrganicSoft.Test.Facturas
             Assert.AreEqual("La nueva cantidad del producto Jabón de Sandía en el carrito es 1", respuesta);
             #endregion
         }
+
+        [Test]
+        public void NoPuedoDisminuirCantidadProductoDelCarrito()
+        {
+
+            #region Dado que laly Organis tiene multiples productos, como jabon de sandia, exfoliante y el cliente cuenta con un carrito de compras que tiene un producto
+            var jabonSandia = new Producto(codigo: 1, nombre: "Jabón de Sandía",
+            decripcion: " Ea hidrante facial y corporal 🍉La sandía es rica en antioxidantes, ayuda a" +
+            " retrasar el envejecimiento de la piel debido a su protección contra los radicales libres." +
+            " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
+            "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
+            costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            jabonSandia.EntradaProductos(cantidad: 10);
+            exfoliante.EntradaProductos(cantidad: 10);
+
+            CarritoCompra carrito = new CarritoCompra(cedulaCliente: "1002353645");
+            ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 1, cantidadVenta: 2);
+            carrito.AgregarAlCarrito(productoVenta);
+
+            #endregion
+            #region CUANDO el cliente desea disminuir la cantidad del producto y dicha cantidad es mayor a la que hay en el carrito actualmente 
+            int cantidadDisminuir = 3;
+            var respuesta = carrito.DisminuirCantidadProducto(codigoProductoVenta: 1, cantidad: cantidadDisminuir);
+            #endregion
+            #region ENTONCES  el sistema no disminuirá la cantidad del producto y mostrará el mensaje "La cantidad a disminuir debe ser menor a la cantidad existente en el carrito"
+            Assert.AreEqual("La cantidad a disminuir debe ser menor a la cantidad existente en el carrito", respuesta);
+            #endregion
+        }
     }
 }
+
 
 internal class ProductoVenta
 {
@@ -268,17 +299,26 @@ internal class CarritoCompra
         }
         foreach (var productoVenta in ProductosVenta)
         {
-            if (codigoProductoVenta.Equals(productoVenta.CodigoProducto) && productoVenta.CantidadVenta > cantidad)
+            if (codigoProductoVenta.Equals(productoVenta.CodigoProducto))
             {
-                productoVenta.DisminuirCantidadProductoVenta(cantidad);
-                foreach (var producto in Producto.Productos)
+                if (productoVenta.CantidadVenta > cantidad)
                 {
-                    if (codigoProductoVenta.Equals(producto.CodigoProducto))
+                    productoVenta.DisminuirCantidadProductoVenta(cantidad);
+                    foreach (var producto in Producto.Productos)
                     {
-
-                        respuesta = $"La nueva cantidad del producto {producto.Nombre} en el carrito es {productoVenta.CantidadVenta}";
+                        if (codigoProductoVenta.Equals(producto.CodigoProducto))
+                        {
+                            return respuesta = $"La nueva cantidad del producto {producto.Nombre} en el carrito es {productoVenta.CantidadVenta}";
+                            
+                        }
                     }
                 }
+                else if (productoVenta.CantidadVenta <= cantidad)
+                {
+                    respuesta = "La cantidad a disminuir debe ser menor a la cantidad existente en el carrito";
+                }
+
+
             }
         }
         
