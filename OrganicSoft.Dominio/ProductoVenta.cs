a52f2f3c67f0ecd78e52824dae6b54768e0c87b9
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-09-29 23:22:58 -0500

    refactorizacion de carrito compro y producto venta

---
+++ OrganicSoft.Dominio/ProductoVenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicSoft.Dominio
+{
+    public class ProductoVenta
+    {
+        public int CodigoProducto { get; private set; }
+        public int CantidadVenta { get; private set; }
+
+        public ProductoVenta(int codigoProducto, int cantidadVenta)
+        {
+            CodigoProducto = codigoProducto;
+            CantidadVenta = cantidadVenta;
+        }
+
+        public int AumentarCantidadProductoVenta(int cantidad)
+        {
+            return CantidadVenta += cantidad;
+        }
+
+        public int DisminuirCantidadProductoVenta(int cantidad)
+        {
+            return CantidadVenta -= cantidad;
+        }
+    }
+}
