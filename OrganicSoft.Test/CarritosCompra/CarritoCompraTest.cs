c485bd5bbecc689961c4acd900d2a41ad91611f9
Aism793 <ismacenteno123@gmail.com>
2021-09-29 21:56:15 -0500

    Caso Prueba No puedo agregar cantidad producto -1 al carrito

--- OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
+++ OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
@@ -11,9 +11,9 @@ namespace OrganicSoft.Test.Facturas
     class CarritoCompraTest
     {
         /*
-      * Como administrador 
-      * quiero confirmar los pedidos de los clientes 
-      * para poder generar las facturas correspondientes a cada pedido
+      * Como usuario 
+      * quiero agregar productos a mi carrito de compras 
+      * para poder generar pedidos
       */
         [Test]
         public void PuedoAgregarProductoAlCarritoCompra()
@@ -44,11 +44,6 @@ namespace OrganicSoft.Test.Facturas
 
         }
 
-        /*
-      * Como administrador 
-      * quiero confirmar los pedidos de los clientes 
-      * para poder generar las facturas correspondientes a cada pedido
-      */
         [Test]
         public void NoExisteProductoParaAgregarAlCarritoCompra()
         {
@@ -74,6 +69,36 @@ namespace OrganicSoft.Test.Facturas
             #endregion
             #region ENTONCES  el sistema mostrará el mensaje "No se encontró el producto"
             Assert.AreEqual("No se encontró el producto", respuesta);
+            #endregion
+
+        }
+
+
+        [Test]
+        public void NoPuedoAgregarCantidadProductoMenosUno()
+        {
+
+            #region Dado que laly Organis tiene multiples productos, como jabon de sandia, exfoliante y el cliente cuenta con un carrito de compras
+            var jabonSandia = new Producto(codigo: 1, nombre: "Jabón de Sandía",
+            decripcion: " Ea hidrante facial y corporal 🍉La sandía es rica en antioxidantes, ayuda a" +
+            " retrasar el envejecimiento de la piel debido a su protección contra los radicales libres." +
+            " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
+            "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
+            costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            jabonSandia.EntradaProductos(cantidad: 10);
+            exfoliante.EntradaProductos(cantidad: 10);
+
+            CarritoCompra carrito = new CarritoCompra(cedulaCliente: "1002353645");
+            ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 3, cantidadVenta: -1);
+
+            #endregion
+            #region CUANDO el cliente desea agregar productos al carrito de compra y la cantidad de productos es -1
+            var respuesta = carrito.AgregarAlCarrito(productoVenta);
+            #endregion
+            #region ENTONCES  el sistema mostrará el mensaje "La cantidad del producto debe ser mayor a cero"
+            Assert.AreEqual("La cantidad del producto debe ser mayor a cero", respuesta);
             #endregion
 
         }
@@ -106,6 +131,11 @@ namespace OrganicSoft.Test.Facturas
         internal string AgregarAlCarrito(ProductoVenta productoVenta)
         {
             String respuesta = "No se encontró el producto";
+            if (productoVenta.CantidadVenta <= 0)
+            {
+                respuesta = "La cantidad del producto debe ser mayor a cero";
+                return respuesta;
+            }
             foreach (var producto in Producto.Productos)
             {
                 if (productoVenta.CodigoProducto.Equals(producto.CodigoProducto))
