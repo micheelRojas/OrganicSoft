b086fa56ed3142e932cc0bf7970a887d9adab9bc
Aism793 <ismacenteno123@gmail.com>
2021-09-29 22:46:01 -0500

    Caso prueba Puedo disminuir cantidad producto del carrito

--- OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
+++ OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
@@ -2,9 +2,6 @@
 using OrganicSoft.Dominio;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrganicSoft.Test.Facturas
 {
@@ -26,14 +23,14 @@ namespace OrganicSoft.Test.Facturas
             " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
             "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
             var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
-            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios", 
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
             costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
             jabonSandia.EntradaProductos(cantidad: 10);
             exfoliante.EntradaProductos(cantidad: 10);
 
             CarritoCompra carrito = new CarritoCompra(cedulaCliente: "1002353645");
             ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 1, cantidadVenta: 2);
-            
+
             #endregion
             #region CUANDO el cliente desea agregar productos al carrito de compra
             var respuesta = carrito.AgregarAlCarrito(productoVenta);
@@ -160,52 +157,90 @@ namespace OrganicSoft.Test.Facturas
             #endregion
         }
 
-        
+        [Test]
+        public void PuedoDisminuirCantidadProductoDelCarrito()
+        {
+
+            #region Dado que laly Organis tiene multiples productos, como jabon de sandia, exfoliante y el cliente cuenta con un carrito de compras que tiene un producto
+            var jabonSandia = new Producto(codigo: 1, nombre: "Jabón de Sandía",
+            decripcion: " Ea hidrante facial y corporal 🍉La sandía es rica en antioxidantes, ayuda a" +
+            " retrasar el envejecimiento de la piel debido a su protección contra los radicales libres." +
+            " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
+            "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
+            costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            jabonSandia.EntradaProductos(cantidad: 10);
+            exfoliante.EntradaProductos(cantidad: 10);
+
+            CarritoCompra carrito = new CarritoCompra(cedulaCliente: "1002353645");
+            ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 1, cantidadVenta: 2);
+            carrito.AgregarAlCarrito(productoVenta);
+
+            #endregion
+            #region CUANDO el cliente desea disminuir la cantidad del producto 
+            int cantidadDisminuir = 1;
+            var respuesta = carrito.DisminuirCantidadProducto(codigoProductoVenta: 1, cantidad: cantidadDisminuir);
+            #endregion
+            #region ENTONCES  el sistema disminuirá la cantidad del producto y mostrará el mensaje "La nueva cantidad del producto Jabón de Sandía en el carrito es 1"
+            Assert.AreEqual("La nueva cantidad del producto Jabón de Sandía en el carrito es 1", respuesta);
+            #endregion
+        }
     }
 }
 
-    internal class ProductoVenta
-    {
-        public int CodigoProducto { get; private set; }
-        public int CantidadVenta { get; private set; }
+internal class ProductoVenta
+{
+    public int CodigoProducto { get; private set; }
+    public int CantidadVenta { get; private set; }
 
-        public ProductoVenta(int codigoProducto, int cantidadVenta)
-        {
-            CodigoProducto = codigoProducto;
-            CantidadVenta = cantidadVenta;
-        }
+    public ProductoVenta(int codigoProducto, int cantidadVenta)
+    {
+        CodigoProducto = codigoProducto;
+        CantidadVenta = cantidadVenta;
     }
 
-    internal class CarritoCompra
+    public int AumentarCantidadProductoVenta(int cantidad)
     {
-        public string CedulaCliente { get; private set; }
-        protected List<ProductoVenta> _productoVentas;
+        return CantidadVenta += cantidad;
+    }
 
-        public CarritoCompra(string cedulaCliente)
-        {
-            CedulaCliente = cedulaCliente;
-            _productoVentas = new List<ProductoVenta>();
-        }
-        public IReadOnlyCollection<ProductoVenta> ProductosVenta => _productoVentas.AsReadOnly();
+    public int DisminuirCantidadProductoVenta(int cantidad)
+    {
+        return CantidadVenta -= cantidad;
+    }
+}
 
-        internal string AgregarAlCarrito(ProductoVenta productoVenta)
+internal class CarritoCompra
+{
+    public string CedulaCliente { get; private set; }
+    protected List<ProductoVenta> _productoVentas;
+
+    public CarritoCompra(string cedulaCliente)
+    {
+        CedulaCliente = cedulaCliente;
+        _productoVentas = new List<ProductoVenta>();
+    }
+    public IReadOnlyCollection<ProductoVenta> ProductosVenta => _productoVentas.AsReadOnly();
+
+    internal string AgregarAlCarrito(ProductoVenta productoVenta)
+    {
+        String respuesta = "No se encontró el producto";
+        if (productoVenta.CantidadVenta <= 0)
         {
-            String respuesta = "No se encontró el producto";
-            if (productoVenta.CantidadVenta <= 0)
-            {
-                respuesta = "La cantidad del producto debe ser mayor a cero";
-                return respuesta;
-            }
-            foreach (var producto in Producto.Productos)
-            {
-                if (productoVenta.CodigoProducto.Equals(producto.CodigoProducto))
-                {
-                    _productoVentas.Add(productoVenta);
-                    respuesta = $"Se ha agregado {productoVenta.CantidadVenta} unidades del producto {producto.Nombre}";
-                }
-            }
+            respuesta = "La cantidad del producto debe ser mayor a cero";
             return respuesta;
         }
+        foreach (var producto in Producto.Productos)
+        {
+            if (productoVenta.CodigoProducto.Equals(producto.CodigoProducto))
+            {
+                _productoVentas.Add(productoVenta);
+                respuesta = $"Se ha agregado {productoVenta.CantidadVenta} unidades del producto {producto.Nombre}";
+            }
+        }
+        return respuesta;
+    }
 
     internal string EliminarDelCarrito(int codigoProductoVenta)
     {
@@ -220,6 +255,33 @@ namespace OrganicSoft.Test.Facturas
                 return respuesta;
             }
         }
+        return respuesta;
+    }
+
+    internal object DisminuirCantidadProducto(int codigoProductoVenta, int cantidad)
+    {
+        String respuesta = "No se encontró el producto";
+        if (cantidad <= 0)
+        {
+            respuesta = "La cantidad a disminuir debe ser mayor a cero";
+            return respuesta;
+        }
+        foreach (var productoVenta in ProductosVenta)
+        {
+            if (codigoProductoVenta.Equals(productoVenta.CodigoProducto) && productoVenta.CantidadVenta > cantidad)
+            {
+                productoVenta.DisminuirCantidadProductoVenta(cantidad);
+                foreach (var producto in Producto.Productos)
+                {
+                    if (codigoProductoVenta.Equals(producto.CodigoProducto))
+                    {
+
+                        respuesta = $"La nueva cantidad del producto {producto.Nombre} en el carrito es {productoVenta.CantidadVenta}";
+                    }
+                }
+            }
+        }
+        
         return respuesta;
     }
 }
