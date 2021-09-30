cf86746794efa376a17e2c037beb94bc2b392a50
Aism793 <ismacenteno123@gmail.com>
2021-09-29 21:49:14 -0500

    Caso Prueba No existe el producto para agregar al carrito

--- OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
+++ OrganicSoft.Test/CarritosCompra/CarritoCompraTest.cs
@@ -43,6 +43,40 @@ namespace OrganicSoft.Test.Facturas
             #endregion
 
         }
+
+        /*
+      * Como administrador 
+      * quiero confirmar los pedidos de los clientes 
+      * para poder generar las facturas correspondientes a cada pedido
+      */
+        [Test]
+        public void NoExisteProductoParaAgregarAlCarritoCompra()
+        {
+
+            #region Dado que laly Organis tiene multiples productos, como jabon de sandia, exfoliante y el cliente cuenta con un carrito de compras
+            var jabonSandia = new Producto(codigo: 1, nombre: "Jabón de Sandía",
+            decripcion: " Ea hidrante facial y corporal 🍉La sandía es rica en antioxidantes, ayuda a" +
+            " retrasar el envejecimiento de la piel debido a su protección contra los radicales libres." +
+            " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
+            "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
+            costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            jabonSandia.EntradaProductos(cantidad: 10);
+            exfoliante.EntradaProductos(cantidad: 10);
+
+            CarritoCompra carrito = new CarritoCompra(cedulaCliente: "1002353645");
+            ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 3, cantidadVenta: 2);
+
+            #endregion
+            #region CUANDO el cliente desea agregar productos al carrito de compra y el producto no existe
+            var respuesta = carrito.AgregarAlCarrito(productoVenta);
+            #endregion
+            #region ENTONCES  el sistema mostrará el mensaje "No se encontró el producto"
+            Assert.AreEqual("No se encontró el producto", respuesta);
+            #endregion
+
+        }
     }
 
     internal class ProductoVenta
