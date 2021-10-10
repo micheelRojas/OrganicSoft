a241733e4f409bb20ce830a8cf358ed6cad4a4b6
Aism793 <ismacenteno123@gmail.com>
2021-10-10 12:57:33 -0500

    Test PuedoGenerarFacturaCorrecta Passed Red

--- OrganicSoft.Test/Factura/FacturaTest.cs
+++ OrganicSoft.Test/Factura/FacturaTest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrganicSoft.Test.Factura
+namespace OrganicSoft.Test.Facturacion
 {
     class FacturaTest
     {
@@ -45,7 +45,63 @@ namespace OrganicSoft.Test.Factura
 
         }
 
-        
+        [Test]
+        public void PuedoGenerarFacturaCorrecta()
+        {
+
+            #region Dado que laly Organis tiene multiples productos, como jabon de sandia, exfoliante y el cliente ha hecho un pedido
+            var jabonSandia = new Producto(codigo: 1, nombre: "Jabón de Sandía",
+            decripcion: " Ea hidrante facial y corporal 🍉La sandía es rica en antioxidantes, ayuda a" +
+            " retrasar el envejecimiento de la piel debido a su protección contra los radicales libres." +
+            " Gracias a estas propiedades, previene los primeros síntomas de la edad, como manchas, " +
+            "arrugas y unas líneas de expresión marcadas.", costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            var exfoliante = new Producto(codigo: 2, nombre: "Exfoliante Mujer",
+            decripcion: "Un exfoliante es un producto hecho principalmente a base de ingredientes naturales que sirve para remover las impurezas y células muertas de los labios",
+            costo: 6000.00, precio: 10000.00, categoria: "Jabon", presentacion: "pequeño, 80 gr", minimoStock: 3);
+            jabonSandia.EntradaProductos(cantidad: 10);
+            exfoliante.EntradaProductos(cantidad: 10);
+
+            CarritoCompra carrito = new CarritoCompra(codigo: 1, cedulaCliente: "1002353645");
+            ProductoVenta productoVenta = new ProductoVenta(codigoProducto: 1, cantidadVenta: 2);
+            carrito.AgregarAlCarrito(productoVenta);
+            ProductoVenta productoVenta2 = new ProductoVenta(codigoProducto: 2, cantidadVenta: 2);
+            carrito.AgregarAlCarrito(productoVenta2);
+            Pedido pedido = new Pedido();
+            pedido.GenerarPedido(codigo: 1, CarritoCompra: carrito);
+            #endregion
+            #region CUANDO el administrador confirma el pedido para generar la factura
+            var respuesta = pedido.ConfirmarPedido(carrito.Codigo);
+            Factura factura = new Factura(codigo: 1, fechaCreacion: DateTime.Now, cedulaCliente: carrito.CedulaCliente);
+            var respuesta2 = factura.CalcularTotal(Pedido: pedido);
+            #endregion
+            #region ENTONCES  el sistema generará la factura con su total a pagar, cambiará el estado del pedido a CONFIRMADO y mostrara el mensaje "El total a pagar es de 20.000 pesos"
+            Assert.AreEqual("El total a pagar es de 20.000 pesos", respuesta2);
+            Assert.AreEqual("El nuevo estado del pedido es CONFIRMADO", respuesta);
+            #endregion
+
+        }
+    }
+
+    internal class Factura
+    {
+        public int Codigo { get; private set; }
+        public DateTime FechaCreacion { get; private set; }
+        public String CedulaCliente { get; private set; }
+        public decimal TotalPagar { get; private set; }
+        protected List<Factura> _facturas;
+
+        public Factura(int codigo, DateTime fechaCreacion, string cedulaCliente)
+        {
+            Codigo = codigo;
+            FechaCreacion = fechaCreacion;
+            CedulaCliente = cedulaCliente;
+            _facturas = new List<Factura>();
+        }
+
+        internal object CalcularTotal(Pedido Pedido)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     internal class Pedido
