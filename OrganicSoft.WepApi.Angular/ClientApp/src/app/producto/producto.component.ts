c52fc4b98ed9bc9a09f20ad660569b4707087a73
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-10-31 13:50:22 -0500

    servicio del producto en IU

--- OrganicSoft.WepApi.Angular/ClientApp/src/app/producto/producto.component.ts
+++ OrganicSoft.WepApi.Angular/ClientApp/src/app/producto/producto.component.ts
@@ -13,3 +13,32 @@ export class ProductoComponent implements OnInit {
   }
 
 }
+export interface IProductoCrear {
+  id: number,
+  codigoProducto: number,
+  nombre: string,
+  descripcion: string,
+  precio: number,
+  categoria: string,
+  presentacion: string,
+  minimoStock: number,
+  componentes: IComponete[],
+  costo: number
+
+}
+export interface IComponete {
+  producto: IProducto,
+  cantidad: number
+}
+export interface IProducto {
+  id: number,
+  codigoProducto: number,
+  nombre: string,
+  descripcion: string,
+  precio: number,
+  categoria: string,
+  presentacion: string,
+  minimoStock: number,
+  costo: number
+
+}
--- OrganicSoft.WepApi.Angular/ClientApp/src/app/producto/producto.service.ts
+++ OrganicSoft.WepApi.Angular/ClientApp/src/app/producto/producto.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { IProducto, IProductoCrear } from './producto.component';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-
-  constructor() { }
+  apiURL = this.baseUrl + "api/Cuenta";
+  private _refresh$ = new Subject<void>();
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+  get refresh$() {
+    return this._refresh$;
+  }
+  getProductos(): Observable<IProducto[]> {
+    return this.http.get<IProducto[]>(this.apiURL);
+  }
+  createProducto(producto: IProductoCrear): Observable<IProductoCrear> {
+    return this.http.post<IProductoCrear>(this.apiURL, producto)
+      .pipe(
+        tap(() => {
+          this._refresh$.next();
+        })
+      );
+  }
 }
+
