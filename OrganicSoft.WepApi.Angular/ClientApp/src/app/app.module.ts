2f96bfa2d4870effec5b9b315d6c7c4fd4a2579f
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-10-31 13:29:09 -0500

    creacion del componete de producto

--- OrganicSoft.WepApi.Angular/ClientApp/src/app/app.module.ts
+++ OrganicSoft.WepApi.Angular/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProductoComponent } from './producto/producto.component';
+import { FormProductoComponent } from './producto/form-producto/form-producto.component';
+import { ListProductoComponent } from './producto/list-producto/list-producto.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProductoComponent,
+    FormProductoComponent,
+    ListProductoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +32,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'registrar-producto', component: FormProductoComponent },
+      { path: 'lista-productos', component: ProductoComponent },
     ])
   ],
   providers: [],
---
+++ OrganicSoft.WepApi.Angular/ClientApp/src/app/producto/list-producto/list-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-producto',
+  templateUrl: './list-producto.component.html',
+  styleUrls: ['./list-producto.component.css']
+})
+export class ListProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
