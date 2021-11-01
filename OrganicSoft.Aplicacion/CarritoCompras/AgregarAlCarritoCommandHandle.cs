2f452055c453b4c6bffe6a79320d9b61d0fe09dc
Aism793 <ismacenteno123@gmail.com>
2021-11-01 18:34:42 -0500

    Agregar al carrito sin controller

---
+++ OrganicSoft.Aplicacion/CarritoCompras/AgregarAlCarritoCommandHandle.cs
@@ -0,0 +1,57 @@
+﻿using OrganicSoft.Dominio;
+using OrganicSoft.Dominio.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicSoft.Aplicacion.CarritoDeCompra
+{
+    public class AgregarAlCarritoCommandHandle
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly ICarritoCompraRepository _carritoCompraRepository;
+        public AgregarAlCarritoCommandHandle(IUnitOfWork unitOfWork, ICarritoCompraRepository carritoCompraRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _carritoCompraRepository = carritoCompraRepository;
+        }
+        public AgregarAlCarritoResponse Handle(AgregarAlCarritoCommand command)
+        {
+
+            var carritoCompra = _carritoCompraRepository.FindFirstOrDefault(carrito => carrito.Id == command.Id);//infraestructura-datos
+            if (carritoCompra == null) return new AgregarAlCarritoResponse("el producto no existe");
+            var response = carritoCompra.AgregarAlCarrito(command.ProductoVenta);//domain
+            _carritoCompraRepository.Update(carritoCompra);//proyectarse el cambio y registrarlo en la unidad de trabajo
+            _unitOfWork.Commit();//infraestructura-datos
+
+            return new AgregarAlCarritoResponse(response);
+        }
+        public class AgregarAlCarritoCommand
+        {
+            public int Id { get; set; }
+            public ProductoVenta ProductoVenta { get; set; }
+        }
+
+        public record AgregarAlCarritoResponse
+        {
+            public AgregarAlCarritoResponse()
+            {
+
+            }
+
+            public AgregarAlCarritoResponse(string mensaje)
+            {
+                Mensaje = mensaje;
+            }
+
+            public string Mensaje { get; set; }
+            public bool isOk()
+            {
+                return this.Mensaje != ("No se pudo añadir el producto al carrito");
+            }
+        }
+    }
+}
