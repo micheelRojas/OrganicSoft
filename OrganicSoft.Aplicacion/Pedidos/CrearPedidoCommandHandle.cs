5c8f462f3bf8cd80833ca53c8ebfc61b4ef44582
Aism793 <ismacenteno123@gmail.com>
2021-11-01 00:25:02 -0500

    Crear Pedido Command Handle

---
+++ OrganicSoft.Aplicacion/Pedidos/CrearPedidoCommandHandle.cs
@@ -0,0 +1,64 @@
+﻿using OrganicSoft.Dominio;
+using OrganicSoft.Dominio.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicSoft.Aplicacion.Pedidos
+{
+    public class CrearPedidoCommandHandle
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPedidoRepository _pedidoRepository;
+        public CrearPedidoCommandHandle(IUnitOfWork unitOfWork, IPedidoRepository pedidoRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _pedidoRepository = pedidoRepository;
+        }
+        public CrearPedidoResponse Handle(CrearPedidoCommand command)
+        {
+            Pedido pedido = _pedidoRepository.FindFirstOrDefault(t => t.Id == command.Id);
+            if (pedido == null)
+            {
+                Pedido pedidoNuevo = new Pedido();
+                pedidoNuevo.GenerarPedido(command.CodigoPedido, command.Carrito);
+                _pedidoRepository.Add(pedidoNuevo);
+                _unitOfWork.Commit();
+                return new CrearPedidoResponse($"Se creó con exito el pedido.");
+            }
+            else
+            {
+                return new CrearPedidoResponse($"La factura ya exite");
+            }
+        }
+    }
+
+    public class CrearPedidoCommand
+    {
+        public int Id { get; private set; }
+        public int CodigoPedido { get; private set; }
+        public CarritoCompra Carrito { get; private set; }
+    }
+
+    public class CrearPedidoResponse
+    {
+        public CrearPedidoResponse()
+        {
+
+        }
+
+        public CrearPedidoResponse(string mensaje)
+        {
+            Mensaje = mensaje;
+        }
+
+        public string Mensaje { get; set; }
+        public bool isOk()
+        {
+            return this.Mensaje.Equals("Se creó con exito el producto.");
+        }
+    }
+}
+
