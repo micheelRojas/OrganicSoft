c328b1f43b8aabb219db7c9413bd8d2069ee1af1
Aism793 <ismacenteno123@gmail.com>
2021-11-01 00:09:17 -0500

    Infraestructura Pedido

---
+++ OrganicSoft.Infraestructura/PedidoRepository.cs
@@ -0,0 +1,19 @@
+﻿using OrganicSoft.Dominio;
+using OrganicSoft.Dominio.Contracts;
+using OrganicSoft.Infraestructura.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicSoft.Infraestructura
+{
+    public class PedidoRepository : GenericRepository<Pedido>, IPedidoRepository
+    {
+        public PedidoRepository(IDbContext context) : base(context)
+        {
+
+        }
+    }
+}
