1c708ec65101cde75091927b93505537feadf141
Aism793 <ismacenteno123@gmail.com>
2021-11-21 23:18:03 -0500

    Cadena de conexión nueva

--- OrganicSoft.WebApi.Angular.Test/Base/CustomWebApplicationFactory.cs
+++ OrganicSoft.WebApi.Angular.Test/Base/CustomWebApplicationFactory.cs
@@ -14,7 +14,7 @@ namespace OrganicSoft.WebApi.Angular.Test.Base
     public class CustomWebApplicationFactory<TStartup>
     : WebApplicationFactory<TStartup> where TStartup : class
     {
-        private readonly string ConnectionString = @"Data Source=DESKTOP-FFMH835\SQLEXPRESS;Initial Catalog=OrganisoftTest;Integrated Security=True;";
+        private readonly string ConnectionString = @"Data Source=DESKTOP-FFMH835\SQLEXPRESS;Initial Catalog=OrganisoftProduction;Integrated Security=True;";
 
         //private readonly string _connectionString = @"Data Source=C:\sqlite\bancoDataBaseEndToEnd.db";
         public OrganicSoftContext CreateContext()
@@ -35,7 +35,7 @@ namespace OrganicSoft.WebApi.Angular.Test.Base
 
                 services.AddDbContext<OrganicSoftContext>(options =>
                 {
-                    options.UseSqlServer(@"Data Source=DESKTOP-FFMH835\SQLEXPRESS;Initial Catalog=OrganisoftTest;Integrated Security=True;");
+                    options.UseSqlServer(@"Data Source=DESKTOP-FFMH835\SQLEXPRESS;Initial Catalog=OrganisoftProduction;Integrated Security=True;");
                 });
                 #endregion
 
