9ec3b7794cbb009ca4e540ddc11632d6d64875dd
micheelRojas <micheel54321@hotmail.com>
2021-11-24 23:41:03 -0500

    prueba de que se ha llega hasta la ventana emergente cuando se intenta gurgar un producto desde IU, falta integrar

---
+++ OrganicSoft.Selenium.Test/ProductoTestSelenium.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace OrganicSoft.Selenium.Test
+{
+    public class Tests
+    {
+        IWebDriver driver;
+        string angularUrl = "http://localhost:4200/";
+        string coreUrl = "https://localhost:5001/";
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = GetDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void ProductosSimples()
+        {
+            driver.Navigate().GoToUrl(angularUrl);
+            NavegarHaciaRegistrarProductos(driver);
+            LlenarFormularioProductoSimple(driver);
+            Assert.IsTrue(driver.FindElement(By.ClassName("swal2-title")).Displayed);
+        }
+        /// Configura el driver para Chrome
+        private IWebDriver GetDriver()
+        {
+            var user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36";
+            ChromeOptions options = new ChromeOptions();
+            //Descomenta esta linea para usar el mode HeadLess de Chrome
+            //options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument($"user_agent={user_agent}");
+            options.AddArgument("--ignore-certificate-errors");
+            IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory(), options);
+            return driver;
+        }
+        private void NavegarHaciaRegistrarProductos(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("opcion-registrar-simple")).Click();
+            System.Threading.Thread.Sleep(100);
+        }
+        private static void LlenarFormularioProductoSimple(IWebDriver driver)
+        {
+           
+            driver.FindElement(By.Id("codigoProducto")).SendKeys("1234");
+            driver.FindElement(By.Id("nombre")).SendKeys("Jabon Banana");
+            driver.FindElement(By.Id("descripcion")).SendKeys("Hidrata y Humeta");
+            driver.FindElement(By.Id("precio")).SendKeys("10000");
+            driver.FindElement(By.Id("categoria")).SendKeys("Jabon");
+            driver.FindElement(By.Id("presentacion")).SendKeys("80 gr Grande");
+            driver.FindElement(By.Id("minimoStock")).SendKeys("1");
+            driver.FindElement(By.Id("costo")).SendKeys("5000");
+            driver.FindElement(By.Id("boton-guardar-simple")).Click();
+            System.Threading.Thread.Sleep(100);
+        }
+    }
+}
